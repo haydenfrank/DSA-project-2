7bccb246dc0dac08cebc3e6bd4495b1ab646ed5b
haydenfrank <haydog2006@gmail.com>
2025-10-28 16:25:42 -0400

    switched switch to a button toggle

--- src/components/ui/user-defined/sortingMethodsSwitch.tsx
+++ src/components/ui/user-defined/sortingMethodsSwitch.tsx
@@ -1,22 +1,31 @@
 "use client";
 
 import * as React from "react";
-import { Switch } from "@/components/ui/switch";
-import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 export function SortingMethodsSwitch() {
   const [isAscending, setIsAscending] = React.useState(true);
+  const handleToggle = (ascending: boolean) => {
+    setIsAscending(ascending);
+    console.log(ascending ? "Sort is ascending" : "Sort is descending");
+  };
 
   return (
-    <div className="flex items-center space-x-2">
-      <Switch
-        id="sort-method"
-        checked={isAscending}
-        onCheckedChange={(checked) => setIsAscending(checked)}
-      />
-      <Label htmlFor="sort-method">
-        {isAscending ? "Ascending" : "Descending"}
-      </Label>
+    <div className="flex items-center">
+      <Button
+        className="rounded-none rounded-l-lg"
+        variant={`${isAscending ? "default" : "outline"}`}
+        onClick={() => handleToggle(true)}
+      >
+        Ascending
+      </Button>
+      <Button
+        className="rounded-none rounded-r-lg"
+        variant={`${isAscending ? "outline" : "default"}`}
+        onClick={() => handleToggle(false)}
+      >
+        Descending
+      </Button>
     </div>
   );
 }
