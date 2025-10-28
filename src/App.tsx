ddf5e5ce3d6df3d2aceeff27387770c303589d5a
haydenfrank <haydog2006@gmail.com>
2025-10-28 13:49:32 -0400

    added three dropdowns and sort button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
+import { Combobox } from "./components/ui/combobox";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +29,13 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <div className="flex justify-center gap-2 md:flex-row mx-auto w-64">
+        <Combobox />
+        <Combobox />
+        <Combobox />
+        <Button>Sort</Button>
+      </div>
     </>
   );
 }
