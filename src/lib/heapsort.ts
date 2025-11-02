57654b48df539537b8b751360a6ab36c755797af
BrandonJBob <BrandonJBob@gmail.com>
2025-11-02 17:08:50 -0500

    fix: heapsort error (#8)
    #fix: heapsort error
    
    ## Changes
    
    - Created a copy of the data at the beginning of the heapSort function
    to prevent the function from modifying the original data.

--- src/lib/heapsort.ts
+++ src/lib/heapsort.ts
@@ -2,7 +2,11 @@
 
 export type graphData = { name: string; value: number }[];
 
-export function heapSort(data: graphData): graphData {
+export function heapSort(input: graphData): graphData {
+  var data: graphData = [];
+  for(var i = 0; i < input.length; i++) {
+    data.push(input[i]);
+  }
   var n = data.length;
   for (var i = Math.floor(n / 2 - 1); i >= 0; i--) {
     data = heapify(data, i, n);
@@ -42,17 +46,14 @@ export function heapify(data: graphData, i: number, n: number): graphData {
   return data;
 }
 
-export function heapTime(data: graphData): {
-  sorted: graphData;
-  time: number;
-} {
+export function heapTime(data: graphData): {sorted: graphData; time: number;} {
   const start = performance.now();
   const sorted = heapSort(data);
   const end = performance.now();
   return { sorted, time: end - start };
 }
 
-// function testing() {
-// var j = (heapSort([{name: "f", value: 6}, {name: "e", value: 5}, {name: "d", value: 3}, {name: "c", value: 1}, {name: "b", value: 2}, {name: "a", value: 4}]));
-// console.log(j);
-// }
+//  function testing() {
+//  var j = (heapSort([{name: "f", value: 6}, {name: "e", value: 5}, {name: "d", value: 3}, {name: "c", value: 1}, {name: "b", value: 2}, {name: "a", value: 4}, {name: "p", value: 0}, {name: "j", value: 10}, {name: "i", value: 9}, {name: "g", value: 7}, {name: "m", value: 72737}, {name: "n", value: 7}, {name: "l", value: 2}, {name: "k", value: 11}, {name: "o", value: 4}, {name: "h", value: 8}]));
+//  console.log(j);
+//  }
