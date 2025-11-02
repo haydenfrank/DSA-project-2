8fe7f5c382d5e9fcd278097e4c888dd0c5b287e8
BrandonJBob <BrandonJBob@gmail.com>
2025-11-02 18:53:26 -0500

    feat: Created heap performance wrapper

--- src/lib/heapsort.ts
+++ src/lib/heapsort.ts
@@ -3,54 +3,88 @@
 export type graphData = { name: string; value: number }[];
 
 export function heapSort(input: graphData): graphData {
+  const c = new Comparator();
   var data: graphData = [];
   for(var i = 0; i < input.length; i++) {
     data.push(input[i]);
   }
   var n = data.length;
   for (var i = Math.floor(n / 2 - 1); i >= 0; i--) {
-    data = heapify(data, i, n);
+    data = heapify(data, i, n, c);
   }
   for (var i = n - 1; i >= 1; i--) {
     var temp = data[i];
     data[i] = data[0];
     data[0] = temp;
-    data = heapify(data, 0, i);
+    data = heapify(data, 0, i, c);
   }
   return data;
 }
 
-export function heapify(data: graphData, i: number, n: number): graphData {
+export function heapify(data: graphData, i: number, n: number, c: Comparator): graphData {
   var root = data[i]["value"];
   var child = 2 * i + 1;
   if (child + 1 < n) {
     var max = Math.max(root, data[child]["value"], data[child + 1]["value"]);
-    if (max != root) {
-      if (max == data[child + 1]["value"]) {
+    c.add(2);
+    if (!c.compare(max, root)) {
+      if (c.compare(max, data[child + 1]["value"])) {
         child++;
       }
       var temp = data[i];
       data[i] = data[child];
       data[child] = temp;
-      return heapify(data, child, n);
+      c.swap();
+      return heapify(data, child, n, c);
     }
   } else if (child < n) {
     var max = Math.max(root, Number(data[child]["value"]));
-    if (max != root) {
+    c.add(1);
+    if (!c.compare(max, root)) {
       var temp = data[i];
       data[i] = data[child];
       data[child] = temp;
-      return heapify(data, child, n);
+      c.swap();
+      return heapify(data, child, n, c);
     }
   }
   return data;
 }
 
-export function heapTime(data: graphData): {sorted: graphData; time: number;} {
+export function heapPerformance(data: graphData): {sorted: graphData; time: number; comparisons: number; swaps: number} {
+  const comp = new Comparator();
+  comp.reset();
   const start = performance.now();
   const sorted = heapSort(data);
   const end = performance.now();
-  return { sorted, time: end - start };
+  const comparisons = comp.getComps();
+  const swaps = comp.getSwaps();
+  return {sorted, time: end - start, comparisons, swaps};
+}
+
+class Comparator {
+  static comps = 0;
+  static swaps = 0;
+  compare(a: number, b: number) {
+    Comparator.comps++;
+    return a == b;
+  }
+  add(n: number) {
+    Comparator.comps += n;
+  }
+  swap() {
+    Comparator.swaps++;
+  }
+  getComps() {
+    return Comparator.comps;
+  }
+  getSwaps() {
+    return Comparator.swaps;
+  }
+  reset() {
+    Comparator.comps = 0;
+    Comparator.swaps = 0;
+  }
 }
 
 //  function testing() {
